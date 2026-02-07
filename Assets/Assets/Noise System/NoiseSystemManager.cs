41fe3ead97748a11cf047785310526301762c0e9
Anometrix <leszek.dopieralo1@gmail.com>
2026-02-07 17:29:05 +0000

    Noise System Changes
    - Added some QoL features into Scriptable Object for Sound System
    - Changed Scriptable Object name from "SoundSystem" to "SoundProfile" for future understanding.
    - Added some math calulation functions within SoundSystemManager Script

--- Assets/Assets/Noise System/NoiseSystemManager.cs
+++ Assets/Assets/Noise System/NoiseSystemManager.cs
@@ -4,13 +4,29 @@ public class NoiseSystemManager : MonoBehaviour
 {
     #region
     // Noise Event Assets
-    [SerializeField] private NoiseSystem playerRun;
-    [SerializeField] private NoiseSystem playerStepOnSomething;
-    [SerializeField] private NoiseSystem openCloseDoor;
-    [SerializeField] private NoiseSystem crashSoft;
-    [SerializeField] private NoiseSystem crashHard;
+    [SerializeField] private NoiseProfile playerRun;
+    [SerializeField] private NoiseProfile playerStepOnSomething;
+    [SerializeField] private NoiseProfile openCloseDoor;
+    [SerializeField] private NoiseProfile crashSoft;
+    [SerializeField] private NoiseProfile crashHard;
 
     // Regular Variables
-    #endregion
-}
 
+    #endregion
+
+    public float CalculateIntensity(NoiseProfile profile, float distance)
+    {
+        float distancefalloff = Mathf.Clamp01(1f - (distance / profile.radius)); // Calculates how much the sound should fall off based on distance and radius
+
+        float finalIntensity = profile.intensity * distancefalloff; // Decides intensity base on distance to noise source
+        finalIntensity *= profile.priority; // Multiplies intensity by priority
+
+        finalIntensity = Mathf.Clamp(finalIntensity, 0f, 50f); // Clamps resault to prevent it from being too loud and prevent bugs
+
+        return finalIntensity;
+    }
+    public float CalculateDistance(Vector3 soundPos, Vector3 listenerPos)
+    {
+        return Vector3.Distance(soundPos, listenerPos);
+    }
+}
