4872e435a358a4c1b0fe2abad0b7f3f207b4b825
Anometrix <leszek.dopieralo1@gmail.com>
2026-04-17 19:52:36 +0100

    fixing

---
+++ Assets/GameAssets/UI_Menu/Scripts/MenuStuffs.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuStuffs : MonoBehaviour
+{
+    [Header("Apps (Menu Options)")]
+    [SerializeField] private GameObject levelSelectApp;
+    [SerializeField] private GameObject settingsApp;
+    [SerializeField] private GameObject quitApp;
+    [SerializeField] private GameObject creditApp;
+    [Header("Credit Menu Options")]
+    [SerializeField] private GameObject creditDevs;
+    [SerializeField] private GameObject creditOther;
+
+
+    #region Open/Close Apps
+    public void OpenCloseLevelSelect()
+    {
+        levelSelectApp.SetActive(!levelSelectApp.activeSelf);
+    }
+    public void OpenCloseSettings()
+    {
+        settingsApp.SetActive(!settingsApp.activeSelf);
+    }
+    public void OpenCloseQuit()
+    {
+        quitApp.SetActive(!quitApp.activeSelf);
+    }
+    public void OpenCloseCredits()
+    {
+        creditApp.SetActive(!creditApp.activeSelf);
+    }
+    #endregion
+
+    public void StartLevel1()
+    {
+        SceneManager.LoadScene("Leszek");
+
+        levelSelectApp.SetActive(false);
+        settingsApp.SetActive(false);
+        quitApp.SetActive(false);
+    }
+    public void SwitchCreditsPanelToDevs()
+    {
+        creditDevs.SetActive(true);
+        creditOther.SetActive(false);
+    }
+    public void SwitchCreditsPanelToOther()
+    {
+        creditDevs.SetActive(false);
+        creditOther.SetActive(true);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
