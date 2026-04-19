1071de1fe95f2a5b04e85b69c3b0d05383145036
Anometrix <leszek.dopieralo1@gmail.com>
2026-04-19 17:46:20 +0100

    Menu Sounds

--- Assets/GameAssets/UI_Menu/Scripts/MenuStuffs.cs
+++ Assets/GameAssets/UI_Menu/Scripts/MenuStuffs.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuStuffs : MonoBehaviour
 {
+    #region Variables
     [Header("Apps (Menu Options)")]
     [SerializeField] private GameObject levelSelectApp;
     [SerializeField] private GameObject settingsApp;
@@ -12,23 +13,37 @@ public class MenuStuffs : MonoBehaviour
     [SerializeField] private GameObject creditDevs;
     [SerializeField] private GameObject creditOther;
     [Header("Other")]
-    [SerializeField] private GameObject otherApp;
+    [SerializeField] private AudioSource buttonClickSource;
+    [SerializeField] private AudioClip buttonClickClip;
+    [SerializeField] private AudioSource menuMusicSource;
+    [SerializeField] private AudioClip menuMusicClip;
+    #endregion
+
+    void Start()
+    {
+        buttonClickSource.clip = buttonClickClip;
+        menuMusicSource.clip = menuMusicClip;
+    }
 
     #region Open/Close Apps
     public void OpenCloseLevelSelect()
     {
+        buttonClickSource.Play();
         levelSelectApp.SetActive(!levelSelectApp.activeSelf);
     }
     public void OpenCloseSettings()
     {
+        buttonClickSource.Play();
         settingsApp.SetActive(!settingsApp.activeSelf);
     }
     public void OpenCloseQuit()
     {
+        buttonClickSource.Play();
         quitApp.SetActive(!quitApp.activeSelf);
     }
     public void OpenCloseCredits()
     {
+        buttonClickSource.Play();
         creditApp.SetActive(!creditApp.activeSelf);
     }
     #endregion
@@ -43,11 +58,13 @@ public class MenuStuffs : MonoBehaviour
     }
     public void SwitchCreditsPanelToDevs()
     {
+        buttonClickSource.Play();
         creditDevs.SetActive(true);
         creditOther.SetActive(false);
     }
     public void SwitchCreditsPanelToOther()
     {
+        buttonClickSource.Play();
         creditDevs.SetActive(false);
         creditOther.SetActive(true);
     }
